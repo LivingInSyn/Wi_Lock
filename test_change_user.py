5d3ed79f69a6fdd3d266f0b7bc64af8f1d9849d5
Jeremy Mill <jeremymill@gmail.com>
2015-06-07 11:07:23 -0400

    house cleaning

--- test_change_user.py
+++
@@ -1,8 +0,0 @@
-import lock_manager
-
-l_manager = lock_manager.Lock_Manager()
-
-#print(l_manager.get_locktime())
-print("trying to change locktime now")
-l_manager.set_locktime(200)
-#print(l_manager.get_locktime())
