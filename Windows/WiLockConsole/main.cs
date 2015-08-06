366e21f1ab9890ab5ce148a8bec8cd50a4866993
Jeremy Mill <jeremymill@gmail.com>
2015-08-06 14:06:46 -0400

    adding the start of the windows stuff

---
+++ Windows/WiLockConsole/main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WiLockConsole
+{
+    public class main
+    {
+        
+        static void Main(string[] args)
+        {
+            wifi_status Status = new wifi_status();
+            while ( true )
+            {
+                Collection<string> ssids = Status.GetSSIDs();
+                foreach (string ssid in ssids)
+                {
+                    Console.WriteLine(ssid);
+                    uint test = GetLastUserInput.GetLastInputTime();
+                    Console.WriteLine(test);
+
+                }
+                //Console.ReadLine();
+                Thread.Sleep(1000);
+                
+            }
+        }
+    }
+}
---
+++ Windows/WiLockConsole/wifi_status.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NativeWifi;
+using System.Collections.ObjectModel;
+
+namespace WiLockConsole
+{
+    public class wifi_status
+    {
+        WlanClient wlan;
+        public wifi_status()
+        {
+            wlan = new WlanClient();
+        }
+
+        public Collection<String> GetSSIDs()
+        {
+            //get the ssids
+            Collection<string> connectedSSIDs = new Collection<string>();
+            
+            foreach (WlanClient.WlanInterface wlanInterface in wlan.Interfaces)
+            {
+                string state = wlanInterface.InterfaceState.ToString();
+                if (state == "Connected")
+                { 
+                    Wlan.Dot11Ssid ssid = wlanInterface.CurrentConnection.wlanAssociationAttributes.dot11Ssid;
+                    connectedSSIDs.Add(new String(Encoding.ASCII.GetChars(ssid.SSID, 0, (int)ssid.SSIDLength)));                  
+                    
+                }
+            }
+            return connectedSSIDs;
+        }
+    }
+}
